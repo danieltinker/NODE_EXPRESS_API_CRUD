ad462927a6b14af5035a50d3a3721689bbb9d58c
Daniel Baruch <danielb534@gmail.com>
2022-05-25 05:41:15 +0300

    first commit

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import {
+  createUser,
+  deleteUser,
+  getUser,
+  getUsers,
+  updateUser,
+} from '../controllers/users.js';
+const router = express.Router();
+
+router.get('/', getUsers);
+
+router.post('/', createUser);
+
+router.get('/:id', getUser);
+
+router.patch('/:id', updateUser);
+
+router.delete('/:id', deleteUser);
+
+export default router;
